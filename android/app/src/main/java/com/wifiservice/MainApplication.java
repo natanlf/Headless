230c61fab1e822e10c4725f8bbba2a7d7b7211e3
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-02-25 11:14:03 -0300

    First commit

---
+++ android/app/src/main/java/com/wifiservice/MainApplication.java
@@ -0,0 +1,59 @@
+package com.wifiservice;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.wifiservice.WifiService;
+import android.content.Context;
+import android.os.Bundle;
+import android.content.Intent;
+import com.facebook.react.HeadlessJsTaskService;
+import android.content.Intent;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+        Intent service = new Intent(getApplicationContext(), WifiService.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString("foo", "bar");
+        service.putExtras(bundle);
+
+        getApplicationContext().startService(service);
+  }
+}
